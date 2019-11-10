6646317ab03051a261d96493d9647bd3a9a3be67
YuChingLi <y8765gd@gmail.com>
2019-11-10 14:02:42 +0800

    add KQUERY from SPOJ

---
+++ KQUERY/README.md
@@ -0,0 +1,5 @@
+# [SPOJ-KQUERY](https://www.spoj.com/problems/KQUERY/)
+
+# AC Other's Solution
+- [gabrielrussoc/competitive-programming](https://github.com/gabrielrussoc/competitive-programming/blob/master/spoj/kquery.cpp)
+- [pushkarmishra/SPOJ](https://github.com/pushkarmishra/SPOJ/blob/master/KQUERY.cpp)
---
+++ KQUERY/kquery_1.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define _ ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+
+
+int main(){
+    int n, num;
+    cin >> n;
+    while(n--){
+        cin >> num;
+        
+    }
+    
+    int n_query;
+    while(n_query--){
+        int i, j, k;
+        cin >> i >> j >> k;
+
+    }
+
+    return 0;
+}
